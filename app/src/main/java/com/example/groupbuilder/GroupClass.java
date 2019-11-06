39c75584f881292bc4ef3e1787817a44bf462faa
Jeremiah Chen <56935165+jeremiahcchen@users.noreply.github.com>
2019-11-06 15:34:14 -0800

    Created the GroupClass that is needed to save information in the future

---
+++ app/src/main/java/com/example/groupbuilder/GroupClass.java
@@ -0,0 +1,51 @@
+package com.example.groupbuilder;
+
+public class GroupClass {
+    String GroupName, GroupLeader;
+    Boolean GroupFree;
+    String[] GroupMem, resource;
+    int memCount;
+
+    public GroupClass() {
+        //Constructor
+    }
+
+    public GroupClass(String GroupName, String GroupLeader, int memCount, Boolean GroupFree,
+                      String[] GroupMem, String[] resource) {
+        this.GroupName = GroupName;
+        this.GroupLeader = GroupLeader;
+        this.memCount = memCount;
+        this.GroupFree = GroupFree;
+        this.GroupMem = GroupMem;
+        this.resource = resource;
+    }
+
+    public void setGroupName(String groupName) { GroupName = groupName; }
+
+    public void setGroupLeader(String groupLeader) { GroupLeader = groupLeader; }
+
+    public void setGroupFree(Boolean groupFree) { GroupFree = groupFree; }
+
+    public void setGroupMem(String[] groupMem) { GroupMem = groupMem; }
+
+    public void setResource(String[] resource) { this.resource = resource; }
+
+    public void setMemCount(int memCount) { this.memCount = memCount; }
+
+
+    public String getGroupName() { return GroupName; }
+
+    public String getGroupLeader() { return GroupLeader; }
+
+    public Boolean getGroupFree() { return GroupFree; }
+
+    public String[] getGroupMem() { return GroupMem; }
+
+    public String[] getResource() { return resource; }
+
+    public int getMemCount() { return memCount; }
+}
+
+
+
+
