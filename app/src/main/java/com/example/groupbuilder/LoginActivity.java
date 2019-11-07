1eeb04535ce1382fdc9fb52b6ca322c6eaa1081d
Daniel Tsai <dinosaurtsai@yahoo.com>
2019-11-06 21:14:03 -0800

    Basic activities for dashboard Two new activities to connect to main dash

--- app/src/main/java/com/example/groupbuilder/LoginActivity.java
+++ app/src/main/java/com/example/groupbuilder/LoginActivity.java
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -74,7 +73,7 @@ public class LoginActivity extends AppCompatActivity {
                 if(task.isSuccessful()){
                     Toast.makeText(getApplicationContext(), "Logged in successfully", Toast.LENGTH_LONG).show();
                     progressBar.setVisibility(View.GONE);
-                    Intent intent = new Intent(LoginActivity.this, dashboard.class);
+                    Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
                     startActivity(intent);
                 }
                 else{
